58fbb6ce4119b3b39cd624546779d226c68ae51c
Aldrich-Fernandes <aldrich.fernandes05@gmail.com>
2022-11-23 17:38:35 +0000

    first commit

---
+++ code/game_data.py
@@ -0,0 +1,98 @@
+level_0 = {
+    'terrain': r'Platformer-(TileMap)/levels/0/level_0_terrain.csv',
+    'coins': r'Platformer-(TileMap)/levels/0/level_0_coins.csv',
+    'fg palms': r'Platformer-(TileMap)/levels/0/level_0_fg palms.csv',
+    'bg palms': r'Platformer-(TileMap)/levels/0/level_0_bg palms.csv',
+    'crates': r'Platformer-(TileMap)/levels/0/level_0_crates.csv',
+    'enemies': r'Platformer-(TileMap)/levels/0/level_0_enemies.csv',
+    'constraints': r'Platformer-(TileMap)/levels/0/level_0_constraints.csv',
+    'player': r'Platformer-(TileMap)/levels/0/level_0_player.csv',
+    'grass': r'Platformer-(TileMap)/levels/0/level_0_grass.csv',
+    'node_pos': (110,400),
+    'unlock': 1,
+    'node_graphics': r'Platformer-(Tilemap)/graphics/overworld/0'
+}
+
+level_1 = {
+    'terrain': r'Platformer-(TileMap)/levels/1/level_1_terrain.csv',
+    'coins': r'Platformer-(TileMap)/levels/1/level_1_coins.csv',
+    'fg palms': r'Platformer-(TileMap)/levels/1/level_1_fg_palms.csv',
+    'bg palms': r'Platformer-(TileMap)/levels/1/level_1_bg_palms.csv',
+    'crates': r'Platformer-(TileMap)/levels/1/level_1_crates.csv',
+    'enemies': r'Platformer-(TileMap)/levels/1/level_1_enemies.csv',
+    'constraints': r'Platformer-(TileMap)/levels/1/level_1_constraints.csv',
+    'player': r'Platformer-(TileMap)/levels/1/level_1_player.csv',
+    'grass': r'Platformer-(TileMap)/levels/1/level_1_grass.csv',
+    'node_pos': (300, 220),
+    'unlock': 2,
+    'node_graphics': r'Platformer-(Tilemap)/graphics/overworld/1'
+}
+
+level_2 = {
+    'terrain': r'Platformer-(TileMap)/levels/2/level_2_terrain.csv',
+    'coins': r'Platformer-(TileMap)/levels/2/level_2_coins.csv',
+    'fg palms': r'Platformer-(TileMap)/levels/2/level_2_fg_palms.csv',
+    'bg palms': r'Platformer-(TileMap)/levels/2/level_2_bg_palms.csv',
+    'crates': r'Platformer-(TileMap)/levels/2/level_2_crates.csv',
+    'enemies': r'Platformer-(TileMap)/levels/2/level_2_enemies.csv',
+    'constraints': r'Platformer-(TileMap)/levels/2/level_2_constraints.csv',
+    'player': r'Platformer-(TileMap)/levels/2/level_2_player.csv',
+    'grass': r'Platformer-(TileMap)/levels/2/level_2_grass.csv',
+    'node_pos': (480, 610),
+    'unlock': 3,
+    'node_graphics': r'Platformer-(Tilemap)/graphics/overworld/2'
+}
+
+level_3 = {
+    'terrain': r'Platformer-(TileMap)/levels/3/level_3_terrain.csv',
+    'coins': r'Platformer-(TileMap)/levels/3/level_3_coins.csv',
+    'fg palms': r'Platformer-(TileMap)/levels/3/level_3_fg_palms.csv',
+    'bg palms': r'Platformer-(TileMap)/levels/3/level_3_bg_palms.csv',
+    'crates': r'Platformer-(TileMap)/levels/3/level_3_crates.csv',
+    'enemies': r'Platformer-(TileMap)/levels/3/level_3_enemies.csv',
+    'constraints': r'Platformer-(TileMap)/levels/3/level_3_constraints.csv',
+    'player': r'Platformer-(TileMap)/levels/3/level_3_player.csv',
+    'grass': r'Platformer-(TileMap)/levels/3/level_3_grass.csv',
+    'node_pos': (610, 350),
+    'unlock': 4,
+    'node_graphics': r'Platformer-(Tilemap)/graphics/overworld/3'
+}
+
+level_4 = {
+    'terrain': r'Platformer-(TileMap)/levels/4/level_4_terrain.csv',
+    'coins': r'Platformer-(TileMap)/levels/4/level_4_coins.csv',
+    'fg palms': r'Platformer-(TileMap)/levels/4/level_4_fg_palms.csv',
+    'bg palms': r'Platformer-(TileMap)/levels/4/level_4_bg_palms.csv',
+    'crates': r'Platformer-(TileMap)/levels/4/level_4_crates.csv',
+    'enemies': r'Platformer-(TileMap)/levels/4/level_4_enemies.csv',
+    'constraints': r'Platformer-(TileMap)/levels/4/level_4_constraints.csv',
+    'player': r'Platformer-(TileMap)/levels/4/level_4_player.csv',
+    'grass': r'Platformer-(TileMap)/levels/4/level_4_grass.csv',
+    'node_pos': (880, 210),
+    'unlock': 5,
+    'node_graphics': r'Platformer-(Tilemap)/graphics/overworld/4'
+}
+
+level_5 = {
+    'terrain': r'Platformer-(TileMap)/levels/2/level_2_terrain.csv',
+    'coins': r'Platformer-(TileMap)/levels/2/level_2_coins.csv',
+    'fg palms': r'Platformer-(TileMap)/levels/2/level_2_fg_palms.csv',
+    'bg palms': r'Platformer-(TileMap)/levels/2/level_2_bg_palms.csv',
+    'crates': r'Platformer-(TileMap)/levels/2/level_2_crates.csv',
+    'enemies': r'Platformer-(TileMap)/levels/2/level_2_enemies.csv',
+    'constraints': r'Platformer-(TileMap)/levels/2/level_2_constraints.csv',
+    'player': r'Platformer-(TileMap)/levels/2/level_2_player.csv',
+    'grass': r'Platformer-(TileMap)/levels/2/level_2_grass.csv',
+    'node_pos': (1050, 400),
+    'unlock': 5,
+    'node_graphics': r'Platformer-(Tilemap)/graphics/overworld/5'
+}
+
+levels = {
+    0: level_0,
+    1: level_1,
+    2: level_2,
+    3: level_3,
+    4: level_4,
+    5: level_5
+}
